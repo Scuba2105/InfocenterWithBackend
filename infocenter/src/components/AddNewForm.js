57c3b970407666a1d698e6d00f4447d926ae33be
Steven Bradbury <scuba2150@gmail.com>
2023-07-10 17:56:23 +1000

    Updates to add new form

--- infocenter/src/components/AddNewForm.js
+++ infocenter/src/components/AddNewForm.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import { Input } from "./Input";
 import { SelectInput } from "./SelectInput"
 
 export function AddNewForm({page, pageData}) {
-    // need img, model, type, manufacturer, 
+    
+    const [addNewManufacturer, setAddNewManufacturer] = useState(false);
+    const [addNewType, setaddNewType] = useState(false);
+
     const placeholderValue = page === "staff" ? "staff member full name" : "equipment model" 
     const nameInputLabel = page === "staff" ? "Full Name" : "Equipment Model/Name"
+
+    function toggleAddNewType() {
+        setaddNewType(t => !t);
+    }
+    
+    function toggleAddNewManufacturer() {
+        setAddNewManufacturer(m => !m);
+    }
 
     function generateExistingSelectValues() {
         const values = pageData.reduce((acc, currEntry) => {
@@ -33,8 +45,22 @@ export function AddNewForm({page, pageData}) {
             <h3 className="add-new-heading" >New Equipment Details</h3>
             <div className="add-new-input-container">
                 <Input inputType="text" identifier="add-new" labelText={nameInputLabel} placeholdertext={`Enter new ${placeholderValue}`} />
-                <SelectInput label="Device Type" optionData={currentTypes} />
-                <SelectInput label="Manufacturer" optionData={currentManufacturers} />
+                <div className="edit-add-new-container">
+                    <button onClick={toggleAddNewType} style={addNewType ? {backgroundColor: '#ff69b4'} : {backgroundColor: 'rgb(16, 172, 105)'}}>
+                        <img className="add-new-edit-button" src={addNewType ? `http://localhost:5000/images/undo.svg` : `http://localhost:5000/images/edit.svg`} alt="edit"></img>
+                    </button>
+                    {addNewType ? <Input inputType="text" identifier="add-new" labelText="Device Type" placeholdertext={`Enter new device type`} /> : 
+                    <SelectInput label="Device Type" optionData={currentTypes} />}
+                    <div className="add-new-aligner"></div>
+                </div>
+                <div className="edit-add-new-container">
+                    <button onClick={toggleAddNewManufacturer} style={addNewManufacturer ? {backgroundColor: '#ff69b4'} : {backgroundColor: 'rgb(16, 172, 105)'}}>
+                        <img className="add-new-edit-button" src={addNewManufacturer ? `http://localhost:5000/images/undo.svg` : `http://localhost:5000/images/edit.svg`} alt="edit"></img>
+                    </button>
+                    {addNewManufacturer ? <Input inputType="text" identifier="add-new" labelText="Device Type" placeholdertext={`Enter new device type`} /> : 
+                    <SelectInput label="Manufacturer" optionData={currentManufacturers} />}
+                    <div className="add-new-aligner"></div>
+                </div>
                 <Input inputType="file" identifier="new-image" labelText="New Device Image" />
             </div>            
             <div className="update-button add-new-upload-button">Upload New Data</div>
