09a8d15fa585f8d8c41b5cb87e7a219ec54d12a7
Steven Bradbury <scuba2150@gmail.com>
2023-07-15 11:17:10 +1000

    Updates to css

--- infocenter/src/components/SearchInput.js
+++ infocenter/src/components/SearchInput.js
@@ -2,19 +2,21 @@ import { SearchIcon } from "../svg";
 
 export function SearchInput({onQueryChange, openAddModal}) {
     return (
-        <div className="search-box">
-            <input style={{
-                            color: "#97a5dd", 
-                            backgroundColor: "transparent", 
-                            border: 'none',
-                            paddingLeft: "50px"
-                        }} 
-                   className="search-input" 
-                   placeholder="Search..."
-                   onChange={onQueryChange}>
-            </input>   
+        <div className="search-box-container">
+            <div className="search-box">
+                <input style={{
+                                color: "#97a5dd", 
+                                backgroundColor: "transparent", 
+                                border: 'none',
+                                paddingLeft: "50px"
+                            }} 
+                    className="search-input" 
+                    placeholder="Search..."
+                    onChange={onQueryChange}>
+                </input>   
+            </div>
             <SearchIcon color="#97a5dd" size="25px" /> 
             <button className="add-new-btn" onClick={openAddModal}>+ Add New</button> 
         </div>
-   );
+    );
 }
