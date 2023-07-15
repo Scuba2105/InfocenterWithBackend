5cb8a2def0bfccb193a490185b91d81398f00d9b
Steven Bradbury <scuba2150@gmail.com>
2023-07-15 15:12:52 +1000

    refactoring backend

--- server.mjs
+++ server.mjs
@@ -3,9 +3,9 @@ import fs from 'fs';
 import path from 'path';
 import cors from 'cors';
 import multer from 'multer';
-import { createDirectory, convertHospitalName, readAllData, readDeviceData, writeDataToFile } from './utils/utils.mjs';
+import { getAllData, updateExistingDeviceData } from './controller/controller.mjs';
+import { createDirectory, convertHospitalName } from './utils/utils.mjs';
 import { capitaliseFirstLetters } from './utils/utils.mjs';
-import { error } from 'console';
 
 // Define the root directory and the port for the server 
 const __dirname = path.dirname('.');
@@ -67,8 +67,7 @@ app.use(express.static('public'))
 
 app.get("/getData", async (req, res) => {
     try {
-        const data = await readAllData(__dirname);
-        res.json(data);
+        await getAllData(req, res);
     } catch (err) {
         console.error(err);
     }
@@ -78,100 +77,11 @@ app.put("/PutDeviceData", async (req, res) => {
     try {
         cpUpload(req, res, async function (err) {
             if (err) {
-                console.log(err.message);
                 res.json({type: "Error", message: err.message});
             }
             else {
-                const allData = await readAllData(__dirname);
-                const deviceData = allData.deviceData;
-
-                // Define the variables from the uploaded data
-                const model = req.body.model;
-                const manufacturer = req.body.manufacturer;
-                let hospital, configPath;
-                const documentKeys = Object.keys(req.body).filter((key) => {
-                    return /description[1-4]/.test(key);
-                });
-                
-                // Retrieve the current data for the updated device
-                const updatedDevice = deviceData.find((entry) => {
-                    return entry.model === model && entry.manufacturer === manufacturer;
-                })
-
-                // Update the device details if corresponding key exists in form data
-                if (Object.keys(req.files).includes('service_manual')) {
-                    updatedDevice.serviceManual = true;
-                }
-
-                if (Object.keys(req.files).includes('user_manual')) {
-                    updatedDevice.userManual = true;
-                }
-
-                if (Object.keys(req.files).includes('configs')) {
-                    const hospital = req.body.hospital;
-                    const hospitalDirectory = convertHospitalName(hospital);
-                    const configPath = `/configurations/${hospitalDirectory}/${model}/${req.files.configs[0].originalname}`
-                    if (Object.keys(updatedDevice.config).includes(hospital)) {
-                        updatedDevice.config[hospital].push(configPath)
-                    } 
-                    else if (updatedDevice.config === "") {
-                        updatedDevice.config = {};
-                        updatedDevice.config[hospital] = [configPath];
-                    }
-                    else {
-                        updatedDevice.config[hospital] = [configPath];
-                    }
-                }
-
-                if (Object.keys(req.body).includes('software')) {
-                    const softwareData = req.body.software;
-                    const softwareDataArray = softwareData.split('=');
-                    const softwareType = softwareDataArray[0];
-                    const softwareLocation = softwareDataArray[1];
-                    
-                    if (typeof updatedDevice.software !== "object") {
-                        updatedDevice.software = {[softwareType]: softwareLocation};
-                    }
-                    else {
-                        updatedDevice.software[softwareType] = softwareLocation;
-                    }
-                }
-            
-                if (documentKeys.length !== 0) {
-                    if (typeof updatedDevice.documents === 'string') {
-                        const documentsInfo = [];
-                        documentKeys.forEach((key, index) => {
-                            const keyObjectDetails = {label: req.body[key], filePath: `/documents/${model}/${req.files[`file${index + 1}`][0].originalname}`} 
-                            documentsInfo.push(keyObjectDetails);
-                        });
-                        updatedDevice.documents = documentsInfo;
-                    }
-                    else {
-                        documentKeys.forEach((key, index) => {
-                            const keyObjectDetails = {label: req.body[key], filePath: `/documents/${model}/${req.files[`file${index + 1}`][0].originalname}`} 
-                            updatedDevice.documents.push(keyObjectDetails);
-                        });
-                    }
-                }
-
-                // Replace the old device data in the DeviceData array with the new data that has been entered 
-                const updatedDeviceData = deviceData.map((entry) => {
-                    if (entry.model === model && entry.manufacturer === manufacturer) {
-                        return updatedDevice
-                    }
-                    else {
-                        return entry;
-                    }
-                })
-
-                const updatedAllData = {staffData: allData.staffData, deviceData: updatedDeviceData} 
-                // Write the data to file
-                writeDataToFile(__dirname, JSON.stringify(updatedAllData, null, 2));
-
-                // Send the updated device's new data as a response.
-                res.json({type: "Success", message: 'Data Upload Successful'});
+                updateExistingDeviceData(req, res);        
             }
-        
         });
     } 
     catch (err) {
