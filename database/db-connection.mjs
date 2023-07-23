f91cdc52ab4407a36c07a5391bc56a44b94c9d49
Steven Bradbury (Hunter New England LHD) <Steven.Bradbury@health.nsw.gov.au>
2023-07-24 08:24:09 +1000

    updates to database queries

--- database/db-connection.mjs
+++ database/db-connection.mjs
@@ -1,8 +1,13 @@
 import { Connection, Request } from "tedious";
+
+// For testing on home laptop DB
 import { localDBConfig } from "../config.mjs";
 
+// For testing on local mobile EMS backend 
+import { localEMSConfig } from "../config.mjs";
+
 export function queryDB(query) {
-  const connection = new Connection(localDBConfig);
+  const connection = new Connection(localEMSConfig);
 
   connection.connect((err) => {
     if (err) {
@@ -10,11 +15,11 @@ export function queryDB(query) {
       throw err;
     }
 
-    executeStatement(query);
+    executeStatement(connection, query);
   });
 }
 
-function executeStatement(query) {
+function executeStatement(connection, query) {
   
   const serialNumberLookup = [];
 
@@ -29,14 +34,19 @@ function executeStatement(query) {
   });
 
   // Emits a 'DoneInProc' event when completed.
+  // This on row callback is for a specific query
   request.on('row', (columns) => {
-    columns.forEach((column) => {
-      if (column.value === null) {
-        console.log('NULL');
-      } else {
-        serialNumberLookup.push(column.value);
+    let rowData = {bme: null, serial: null};
+    columns.forEach((column, index) => {
+      if (index === 0) {
+        rowData.bme = column.value;
       }
+      else if (index === 1) {
+        rowData.serial = column.value;
+      }
+      else (console.log("unknown column"));
     });
+    serialNumberLookup.push(rowData);
   });
 
   request.on('done', (rowCount) => {
@@ -50,3 +60,5 @@ function executeStatement(query) {
   // Execute the define statement
   connection.execSql(request);
 }
+
+//queryDB("SELECT BMENO, SERIAL_NO FROM tblEquipment WHERE LocationCurrent = 294 AND MANUFACT LIKE '%SLE%'");
