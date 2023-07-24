96b81dc33ff698c15e4aeff12c2dacb803712b83
Steven Bradbury <scuba2150@gmail.com>
2023-07-24 18:31:34 +1000

    updates to database

--- database/db-connection.mjs
+++ database/db-connection.mjs
@@ -1,17 +1,24 @@
 import sql from "mssql";
+import { localDBConfig } from "../config.mjs";
 import { localEMSConfig } from "../config.mjs";
 
-async function queryDB() {
+async function queryDB(parameter) {
   try {
       // make sure that any items are correctly URL encoded in the connection string
-      await sql.connect(localEMSConfig);
-      const result = await sql.query`SELECT BMENO, SERIAL_NO FROM tblEquipment WHERE LocationCurrent = 294 AND MANUFACT LIKE '%SLE%'`
+      await sql.connect(localDBConfig);
+      const result = await sql.query(`SELECT * FROM Equipment WHERE BMENO IN ${parameter}`);
       return result
   } catch (err) {
       // ... error checks
   }
 }
 
-const serialBMELookup = await queryDB();
-console.log(serialBMELookup.recordset);
+const array = ["37646", "57565", "45742", "55406", "53729", "37746", "54024", "51784", "47767"];
+const lastIndex = array.length - 1;
+const queryParameter = array.map((bme, index) => {
+  return index === 0 ? `('${bme}'` : index === lastIndex ? `'${bme}')` : `'${bme}'`;
+}).join(",");
+
+const serialBMELookup = await queryDB(queryParameter);
+console.log(serialBMELookup);
 
