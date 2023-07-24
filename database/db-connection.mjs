d910a5e61d4f0bf7b766f0f7512ae346c3cd0be7
Steven Bradbury (Hunter New England LHD) <Steven.Bradbury@health.nsw.gov.au>
2023-07-24 11:18:01 +1000

    Updated db file with mssql

--- database/db-connection.mjs
+++ database/db-connection.mjs
@@ -1,64 +1,18 @@
-import { Connection, Request } from "tedious";
-
-// For testing on home laptop DB
-import { localDBConfig } from "../config.mjs";
-
-// For testing on local mobile EMS backend 
+import sql from "mssql";
 import { localEMSConfig } from "../config.mjs";
 
-export function queryDB(query) {
-  const connection = new Connection(localEMSConfig);
-
-  connection.connect((err) => {
-    if (err) {
-      console.log('Connection Failed');
-      throw err;
-    }
-
-    executeStatement(connection, query);
-  });
+async function queryDB() {
+  try {
+      // make sure that any items are correctly URL encoded in the connection string
+      await sql.connect(localEMSConfig);
+      const result = await sql.query`SELECT BMENO, SERIAL_NO FROM tblEquipment WHERE LocationCurrent = 294 AND MANUFACT LIKE '%SLE%'`
+      return result
+  } catch (err) {
+      // ... error checks
+  }
 }
 
-function executeStatement(connection, query) {
-  
-  const serialNumberLookup = [];
-
-  const request = new Request(query, (err, rowCount) => {
-    if (err) {
-      throw err;
-    }
-
-    console.log(serialNumberLookup);
-    console.log('DONE!');
-    connection.close();
-  });
-
-  // Emits a 'DoneInProc' event when completed.
-  // This on row callback is for a specific query
-  request.on('row', (columns) => {
-    let rowData = {bme: null, serial: null};
-    columns.forEach((column, index) => {
-      if (index === 0) {
-        rowData.bme = column.value;
-      }
-      else if (index === 1) {
-        rowData.serial = column.value;
-      }
-      else (console.log("unknown column"));
-    });
-    serialNumberLookup.push(rowData);
-  });
-
-  request.on('done', (rowCount) => {
-    console.log('Done is called!');
-  });
-
-  request.on('doneInProc', (rowCount, more) => {
-    console.log(rowCount + ' rows returned');
-  });
-
-  // Execute the define statement
-  connection.execSql(request);
-}
+const serialBMELookup = await queryDB();
+console.log(serialBMELookup.recordset);
 
 //queryDB("SELECT BMENO, SERIAL_NO FROM tblEquipment WHERE LocationCurrent = 294 AND MANUFACT LIKE '%SLE%'");
