e16db8a6f208a3d5a8a4b0c253b9caafe08ccf09
Steven Bradbury <scuba2150@gmail.com>
2023-07-30 11:31:43 +1000

    updates to thermometer returns form

--- infocenter/src/components/ThermometerModal.js
+++ infocenter/src/components/ThermometerModal.js
@@ -1,17 +1,26 @@
 export function ThermometerModal({batchData, type}) {
     
+    const date = new Date(batchData[0].date);
+    const dateString = date.toLocaleDateString();
+
     if (type === "check") {
         return (
             <div className="thermometer-modal-container">
-                {batchData.map((entry) => {
-                    return (
-                        <div className="check-data-container">
-                            <label>{`BME #: ${entry.bme}`}</label>
-                            <label>{`Serial #: ${entry.serial}`}</label>
-                            <input type="checkbox"></input>
-                        </div> 
-                    );
-                })}
+                <h4 id="thermometer-batch-heading">{`Thermometer Batch ${dateString}`}</h4>
+                <p id="thermometer-batch-info">*The following list of thermometer have not yet been received back from repair/calibration.</p>
+                <div className="thermometer-list-container">
+                    {batchData.map((entry, index) => {
+                        return (
+                            <div key={`check-data-container${index}`} className="check-data-container">
+                                <label id="thermometer-bme">{`BME #: ${entry.bme}`}</label>
+                                <label id="thermometer-serial">{`Serial #: ${entry.serial}`}</label>
+                                <div id="thermometer-checkbox">
+                                    <input type="checkbox"></input>
+                                </div>
+                            </div> 
+                        );
+                    })}
+                </div>
             </div>
         )
     }
