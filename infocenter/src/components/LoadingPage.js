1d4eb99f9ac5e08ccb39b3f16c94947d29b96826
Steven Bradbury <scuba2150@gmail.com>
2023-08-10 21:39:20 +1000

    prototype loading page

--- infocenter/src/components/LoadingPage.js
+++ infocenter/src/components/LoadingPage.js
@@ -3,7 +3,7 @@ export function LoadingPage() {
     return (
         <div className="loading-page-container">
             <div class="loader"></div>
-            <h1>{`Loading App Data. Please Wait...`}</h1>
+            <h2>{`Loading App Data. Please Wait...`}</h2>
         </div>
     );
 }
