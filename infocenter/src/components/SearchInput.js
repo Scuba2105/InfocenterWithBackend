e8d26409844007e749b6cf9bcbfdec53abc47d82
Steven Bradbury <scuba2150@gmail.com>
2023-08-19 17:20:06 +1000

    updates to css

--- infocenter/src/components/SearchInput.js
+++ infocenter/src/components/SearchInput.js
@@ -2,6 +2,31 @@ import { SearchIcon } from "../svg";
 import { useUser } from "./StateStore";
 import useMediaQueries from "media-queries-in-react";
 
+function searchBoxTransform(laptop) {
+    if (laptop) {
+        return {width: 100 + '%', transform: 'translateX(12px)'}
+    }
+    else {
+        return {width: 100 + '%', transform: 'translateX(13px)'}
+    }
+}
+
+function searchIconClass(laptop, permissions) {
+    if (laptop && permissions === "user") {
+        return "search-icon-laptop-user";
+    }
+    else if (laptop && permissions === "admin") {
+        return "search-icon-laptop-admin";
+    }
+    else if (!laptop && permissions === "user") {
+        return "search-icon-desktop-user";
+    }
+    else {
+        return "search-icon-desktop-admin";
+    }
+    
+}
+
 export function SearchInput({onQueryChange, openAddModal}) {
 
     const mediaQueries = useMediaQueries({
@@ -12,9 +37,11 @@ export function SearchInput({onQueryChange, openAddModal}) {
     // Get user state from Zustand state
     const currentUser = useUser((state) => state.userCredentials);
     
+    const searchIconClassName = searchIconClass(mediaQueries.laptop, currentUser.permissions)
+
     return (
         <div className="search-box-container">
-            <div className={mediaQueries.laptop ? "search-box-laptop" : "search-box-desktop"}>
+            <div className={mediaQueries.laptop ? "search-box-laptop" : "search-box-desktop"} style={currentUser.permissions !== "admin" ? searchBoxTransform(mediaQueries.laptop) : null}>
                 <input style={{
                                 color: "#97a5dd", 
                                 backgroundColor: "transparent", 
@@ -26,8 +53,8 @@ export function SearchInput({onQueryChange, openAddModal}) {
                     onChange={onQueryChange}>
                 </input>   
             </div>
-            <SearchIcon color="#97a5dd" size="25px" mediaQueries={mediaQueries}/> 
-            {currentUser.permissions === "user" && <button className={mediaQueries.laptop ? "add-new-btn-laptop" : "add-new-btn-desktop"} onClick={openAddModal}>+ Add New</button>} 
+            <SearchIcon color="#97a5dd" size="25px" searchIconClassName={searchIconClassName}/> 
+            {currentUser.permissions === "admin" && <button className={mediaQueries.laptop ? "add-new-btn-laptop" : "add-new-btn-desktop"} onClick={openAddModal}>+ Add New</button>} 
         </div>
     );
 }
