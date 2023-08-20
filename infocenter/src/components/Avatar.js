9085394ba7c641e27e7a86023d9dd17c7ac89157
Steven Bradbury <scuba2150@gmail.com>
2023-08-20 10:50:18 +1000

    working on avatar menu

--- infocenter/src/components/Avatar.js
+++ infocenter/src/components/Avatar.js
@@ -1,11 +1,17 @@
 import { serverConfig } from "../server";
 import { useUser } from "./StateStore";
 import { useState } from "react";
+import useMediaQueries from "media-queries-in-react";
 
 export function Avatar() {
 
     const [menuVisible, setMenuVisible] = useState(false);
     const currentUser = useUser((state) => state.userCredentials);
+
+    const mediaQueries = useMediaQueries({
+        laptop: "(max-width: 1250px)",
+        desktop: "(min-width: 1800px)"
+    });
 
     function toggleMenu() {
         setMenuVisible(m => !m)
@@ -15,8 +21,8 @@ export function Avatar() {
         <div className="avatar-container" onClick={toggleMenu}>
             <img id="avatar-image" src={`http://${serverConfig.host}:${serverConfig.port}/images/staff/blank-profile.png`} alt="staff"></img>
             <label>{currentUser.user}</label>
-            <div className="avatar-menu" style={menuVisible ? {opacity: 1} : {opacity: 0}}>
-                <label>{currentUser.permissions}</label>
+            <div className={mediaQueries.laptop ? "avatar-menu-laptop" : "avatar-menu avatar-menu-desktop"} style={menuVisible ? {opacity: 1} : {opacity: 0}}>
+                <label>{currentUser.permissions[0].toUpperCase() + currentUser.permissions.split("").slice(1).join("")}</label>
                 <label>Change Password</label>
                 <label>Logout</label>
             </div>
