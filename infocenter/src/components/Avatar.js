4ce3af553f08bcb34584618ff6436c23f5ff87fc
Steven Bradbury <scuba2150@gmail.com>
2023-08-20 12:46:38 +1000

    prototype for avatar menu

--- infocenter/src/components/Avatar.js
+++ infocenter/src/components/Avatar.js
@@ -1,12 +1,21 @@
 import { serverConfig } from "../server";
-import { useUser } from "./StateStore";
+import { useUser, useLoggedIn } from "./StateStore";
 import { useState } from "react";
 import useMediaQueries from "media-queries-in-react";
+import { PadlockIcon, LogoutIcon } from "../svg";
+
+function logoutFromApp(logout) {
+    sessionStorage.removeItem("currentInfoCentreSession");
+    logout();
+}
 
 export function Avatar() {
 
     const [menuVisible, setMenuVisible] = useState(false);
     const currentUser = useUser((state) => state.userCredentials);
+
+    // Get the logout function from the state store
+    const logout = useLoggedIn((state) => state.logout);
 
     const mediaQueries = useMediaQueries({
         laptop: "(max-width: 1250px)",
@@ -22,9 +31,15 @@ export function Avatar() {
             <img id="avatar-image" src={`http://${serverConfig.host}:${serverConfig.port}/images/staff/blank-profile.png`} alt="staff"></img>
             <label>{currentUser.user}</label>
             <div className={mediaQueries.laptop ? "avatar-menu-laptop" : "avatar-menu avatar-menu-desktop"} style={menuVisible ? {opacity: 1} : {opacity: 0}}>
-                <label>{currentUser.permissions[0].toUpperCase() + currentUser.permissions.split("").slice(1).join("")}</label>
-                <label>Change Password</label>
-                <label>Logout</label>
+                <label id="permission-label">{currentUser.permissions === "admin" ? "Administrator" : currentUser.permissions[0].toUpperCase() + currentUser.permissions.split("").slice(1).join("")}</label>
+                <div className="avatar-option">
+                    <PadlockIcon color="white" size="25px"></PadlockIcon>
+                    <label id="change-password">Change Password</label>
+                </div>
+                <div className="avatar-option" onClick={() => logoutFromApp(logout)}>
+                    <LogoutIcon color="white" size="25px"></LogoutIcon>
+                    <label id="logout-label">Logout</label>
+                </div>
             </div>
         </div>
     )
