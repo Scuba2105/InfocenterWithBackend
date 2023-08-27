33d0d3078a27493fab3e16299e5d7309410ac66b
Steven Bradbury <scuba2150@gmail.com>
2023-08-27 13:28:35 +1000

    updates to contact page

--- infocenter/src/components/ContactCard.js
+++ infocenter/src/components/ContactCard.js
@@ -1,10 +1,24 @@
 export function ContactCard({contact}) {
     return (
         <div className="contact-card-container">
-            <label>{contact.contact}</label>
-            <label>{contact.position}</label>
-            <label>{`Office Ph: ${contact.officePhone}`}</label>
-            {contact.dectPhone && <label>{`Dect Ph: ${contact.dectPhone}`}</label>}
+            <div className="contact-card-header">
+                <label id="contact-name">{contact.contact}</label>
+                <label id="contact-position">{contact.position}</label>
+            </div> 
+            <div className="contact-numbers-container">
+                <div className="contact-number">
+                    <label className="number-description">Office Phone</label>
+                    <label className="number-value">{contact.officePhone}</label>
+                </div>
+                <div className="contact-number">
+                    <label className="number-description">Dect Phone</label>
+                    <label className="number-value">{contact.dectPhone ? contact.dectPhone : "-"}</label>
+                </div>
+                <div className="contact-number">
+                    <label className="number-description">Mobile Phone</label>
+                    <label className="number-value">{contact.mobilePhone ? contact.mobilePhone : "-"}</label>
+                </div>                
+            </div>   
         </div>
     );
 }
--- infocenter/src/components/ContactsSummary.js
+++ infocenter/src/components/ContactsSummary.js
@@ -1,18 +1,45 @@
+import { useState } from "react";
+import { NextIcon } from "../svg";
 import { ContactCard } from "./ContactCard";
 import { ContactsFilter } from "./ContactsFilter";
 import useMediaQueries from "media-queries-in-react"
 
 export function ContactsSummary({selectedDepartment, pageData, onHospitalChange, onDepartmentChange}) {
     
+    const [page, setPage] = useState(0);
+
     const mediaQueries = useMediaQueries({
         laptop: "(max-width: 1250px)",
         desktop: "(min-width: 1800px)"
     });
 
-    const departmentContacts = pageData.filter((entry) => {
+    const allDepartmentContacts = pageData.filter((entry) => {
         return entry.hospital === selectedDepartment.hospital && entry.department === selectedDepartment.department
     });
+
+    // Define the number of contacts to display per page and the max page number
+    const entriesPerPage = 2;
+    const maxIndex = Math.ceil(allDepartmentContacts.length / entriesPerPage - 1);
+
+    // Filter the department contacts to show only those on the current page
+    const displayedDepartmentContacts = allDepartmentContacts.slice(page*entriesPerPage, page*entriesPerPage + entriesPerPage);
     
+    function pageArrowClick(e) {
+        let id = e.target.id;
+        while (!id) {
+            id = e.target.parentNode.id;
+        }
+        
+        const pressed = id.split('_')[0];
+        
+        if (pressed === "forward-next" && page < maxIndex) {
+            setPage(p => p + 1);
+        } 
+        else if (pressed === "back-next" && page > 0) {
+            setPage(p => p - 1);
+        }
+    }
+
     return (
         <div className="hospital-contacts-container">
             <div className="contacts-heading">
@@ -21,13 +48,18 @@ export function ContactsSummary({selectedDepartment, pageData, onHospitalChange,
             <div className={mediaQueries.laptop ? "contacts-main-display contacts-main-display-laptop" : "contacts-main-display contacts-main-display-desktop"}>
                 <ContactsFilter selectedDepartment={selectedDepartment} pageData={pageData} onHospitalChange={onHospitalChange} onDepartmentChange={onDepartmentChange} ></ContactsFilter>
                 <div className="contacts-display">
-                    {departmentContacts.map((contact) => {
+                    {displayedDepartmentContacts.map((contact) => {
                         return (
                             <div className="contact-container">
                                 <ContactCard  contact={contact}></ContactCard>
                             </div>
                         )
                     })}
+                </div>
+                <div className="page-controls" onClick={pageArrowClick}>
+                    <NextIcon className="back-next-icon" color="white" size="11px" offset="1" angle="180" id="back-next" />
+                    <label className="table-page-info">{`Page ${page + 1} of ${maxIndex + 1}`}</label>
+                    <NextIcon className="forward-next-icon" color="white" size="11px" offset="0" angle="0" id="forward-next" />
                 </div>
             </div>                  
         </div>
