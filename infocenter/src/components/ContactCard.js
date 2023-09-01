6b1e80af73317eeba8fe4629419a7045c36b3004
Steven Bradbury (Hunter New England LHD) <Steven.Bradbury@health.nsw.gov.au>
2023-09-01 10:01:58 +1000

    fiexed GE Portal link

--- infocenter/src/components/ContactCard.js
+++ infocenter/src/components/ContactCard.js
@@ -24,7 +24,7 @@ export function ContactCard({identifier, contact, index}) {
                 </div> 
                 {identifier === "vendor" && <div className="contact-number">
                     <label className="number-description">{contact.email === "https://services.gehealthcare.com.au/gehcstorefront/" ? "GE Portal" : "Email" }</label>
-                    <a className="number-value" href={contact.email === "-" ? null : contact.email === "https://services.gehealthcare.com.au/gehcstorefront/" ? contact.email : `mailto:${contact.email}`}>{contact.email === "https://services.gehealthcare.com.au/gehcstorefront/" ? "Website" : contact.email}</a>
+                    <a className="number-value website-link" href={contact.email === "-" ? null : contact.email === "https://services.gehealthcare.com.au/gehcstorefront/" ? contact.email : `mailto:${contact.email}`}>{contact.email === "https://services.gehealthcare.com.au/gehcstorefront/" ? "Website" : contact.email}</a>
                 </div>}                
             </div>   
         </div>
