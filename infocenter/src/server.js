ef5a63d6925f7b3d3a9cab8f3445c7895c8c2025
Steven Bradbury (Hunter New England LHD) <Steven.Bradbury@health.nsw.gov.au>
2023-09-25 16:10:41 +1000

    updates to bug fixes

--- infocenter/src/server.js
+++ infocenter/src/server.js
@@ -1,5 +1,6 @@
-// Need to update hostname when deployed
+// Need to update hostname when deployed for prod. 
+//***** Make sure to change to local host for local dev
 export const serverConfig = {
-    host: 'localhost',
+    host: 'JHHBME005',
     port: 5000
 }
