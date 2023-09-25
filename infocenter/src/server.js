398fc44641b7c942906e7a5d2c867b6af29e775e
Steven Bradbury <scuba2150@gmail.com>
2023-09-25 17:15:21 +1000

    removed server.js from git so it doesnt change between work dev and home dev

--- infocenter/src/server.js
+++ infocenter/src/server.js
@@ -1,6 +1,6 @@
 // Need to update hostname when deployed for prod. 
 //***** Make sure to change to local host for local dev
 export const serverConfig = {
-    host: 'JHHBME005',
+    host: 'localhost',
     port: 5000
 }
