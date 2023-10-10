e4a0e5d18550b85540cb526f7c8f78632cd35f2d
Steven Bradbury <scuba2150@gmail.com>
2023-10-10 23:22:25 +1100

    updates to calendar

--- infocenter/src/components/CalendarComponent.js
+++ infocenter/src/components/CalendarComponent.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 
+const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
+
 /* Import the on-call roster
 Kendo Wu - 09/10/2023
 Matthew Murrell - Please divert phone to 0419295532
@@ -13,19 +15,19 @@ Ray Aunei Mose
 Rodney Birt
 */
 
-export function CalendarComponent({date, setDate}) {
+export function CalendarComponent() {
   const [date, setDate] = useState(new Date());
 
+  const currentDate = new Date();
   return (
-    <div className='on-call-page'>
-      <h1 className='text-center'>React Calendar</h1>
-      <div className='calendar-container'>
-        <Calendar onChange={setDate} value={date} />
-      </div>
-      <p className='text-center'>
-        <span className='bold'>Selected Date:</span>{' '}
-        {date.toDateString()}
-      </p>
+    <div className='calendar-half-page'>
+        <div className='calendar-container'>
+            <p className='calendar-header'>
+                <span className="year">{currentDate.toLocaleDateString('en-us', options).split(', ')[2]}</span>
+                <span className="day-month">{currentDate.toLocaleDateString('en-us', options).split(', ').slice(0,2).join(', ')}</span>
+            </p>      
+            <Calendar onChange={setDate} value={date} />
+        </div>
     </div>
   );
 }
