88ce009d6830b2268807bb48ad60e9ffc3a937df
Steven Bradbury <scuba2150@gmail.com>
2023-10-13 22:32:24 +1100

    added on-call function svg icons

--- infocenter/src/components/OnCallFunctions.js
+++ infocenter/src/components/OnCallFunctions.js
@@ -1,10 +1,10 @@
-import { EditIcon } from "../svg"
+import { EditRosterIcon, MyRosterIcon, StaffRosterIcon, KeyContactsIcon, CheatsheetIcon } from "../svg"
 
 // Edit roster, My On-Call, Staff Roster, Key Contacts, On-Call Cheatsheet  
 export function OnCallFunctions() {
     return (
         <div className="on-call-functions-container">
-        
+            
         </div>
     )
 }
