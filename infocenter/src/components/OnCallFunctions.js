634c28298aeb60f9c05168ee5815ab36b80c2b90
Steven Bradbury <scuba2150@gmail.com>
2023-10-13 21:36:59 +1100

    updates to on-call

--- infocenter/src/components/OnCallFunctions.js
+++ infocenter/src/components/OnCallFunctions.js
@@ -1,6 +1,10 @@
+import { EditIcon } from "../svg"
+
+// Edit roster, My On-Call, Staff Roster, Key Contacts, On-Call Cheatsheet  
 export function OnCallFunctions() {
     return (
-        <>
-        </>
+        <div className="on-call-functions-container">
+        
+        </div>
     )
 }
