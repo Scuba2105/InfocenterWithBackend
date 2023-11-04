8c4a2ab800558a7085f6532d3a6b9810e25b4eeb
Steven Bradbury <scuba2150@gmail.com>
2023-11-04 12:11:54 +1100

    added permissions to on-call page

--- infocenter/src/components/OnCall/OnCallFunctions.js
+++ infocenter/src/components/OnCall/OnCallFunctions.js
@@ -1,4 +1,5 @@
-import { EditRosterIcon, ConfirmRosterIcon, MyRosterIcon, StaffRosterIcon, CheatsheetIcon, AuthorisationIcon } from "../../svg"
+import { EditRosterIcon, ConfirmRosterIcon, MyRosterIcon, StaffRosterIcon, 
+    CheatsheetIcon, AuthorisationIcon, PadlockIcon } from "../../svg"
 import { ModalSkeleton } from "../ModalSkeleton";
 import { MyOnCall } from "./MyOnCall";
 import { StaffRoster } from "./StaffRoster";
@@ -6,14 +7,18 @@ import { useState } from "react";
 import { EditRoster } from "./EditRoster";
 import { ConfirmRoster } from "./ConfirmRoster";
 import { serverConfig } from "../../server";
+import { useUser } from "../StateStore";
+
+// On-Call Administrators
+const onCallAdministrators = ["Paul Cookson", "Durga Sompalle", "Atif Siddiqui"];
 
 // Define the labels and icons for mapping the buttons.
-const functionsData = [{label: "Edit Roster", "icon": EditRosterIcon, color: "#5ef8ed"},
-{label: "Confirm Roster", "icon": ConfirmRosterIcon, color: "#BCE7FD"},
-{label: "My Roster", "icon": MyRosterIcon, color: "#BCE7FD"}, 
-{label: "Staff Roster", "icon": StaffRosterIcon, color: "#5ef8ed"}, 
-{label: "On-Call Cheatsheet", "icon": CheatsheetIcon, color: "#5ef8ed", "href": `https://${serverConfig.host}:${serverConfig.port}/oncall/On Call Cheat Sheet.docx`}, 
-{label: "Authorisation Form", "icon": AuthorisationIcon, color: "#BCE7FD", "href": `https://${serverConfig.host}:${serverConfig.port}/oncall/Callback Authorisation Form.doc`}];
+const functionsData = [{label: "Edit Roster", "icon": EditRosterIcon, color: "#5ef8ed", restricted: true},
+{label: "Confirm Roster", "icon": ConfirmRosterIcon, color: "#BCE7FD", restricted: true},
+{label: "My Roster", "icon": MyRosterIcon, color: "#BCE7FD", restricted: false}, 
+{label: "Staff Roster", "icon": StaffRosterIcon, color: "#5ef8ed", restricted: false}, 
+{label: "On-Call Cheatsheet", "icon": CheatsheetIcon, color: "#5ef8ed", restricted: false, "href": `https://${serverConfig.host}:${serverConfig.port}/oncall/On Call Cheat Sheet.docx`}, 
+{label: "Authorisation Form", "icon": AuthorisationIcon, color: "#BCE7FD", restricted: false, "href": `https://${serverConfig.host}:${serverConfig.port}/oncall/Callback Authorisation Form.doc`}];
 
 const modalLinkButtons = ["my-roster", "edit-roster", "staff-roster", "confirm-roster"];
 
@@ -34,6 +39,9 @@ export function OnCallFunctions({queryClient, showMessage, closeDialog, page}) {
     const [formVisible, setFormVisibile] = useState(false);
     const [formType, setFormType] = useState(null);
 
+    // Get the current user
+    const currentUser = useUser((state) => state.userCredentials);
+
     return (
         <div className="on-call-functions-container">
             <div className="on-call-functions-header flex-c">On-Call Utilities</div>
@@ -43,8 +51,8 @@ export function OnCallFunctions({queryClient, showMessage, closeDialog, page}) {
                 if (modalLinkButtons.includes(type)) {
                     return (
                         <div key={`${entry.label}-key`} className="on-call-function-container flex-c size-100">
-                            <button className={`on-call-function-button flex-c-col ${type} ${design}`} onClick={() => showForm(setFormVisibile, setFormType, type)}>
-                                <entry.icon color={entry.color} size="2.7vw"></entry.icon>
+                            <button className={`on-call-function-button flex-c-col ${type} ${design}`} onClick={!entry.restricted || (entry.restricted && onCallAdministrators.includes(currentUser.user)) ? () => showForm(setFormVisibile, setFormType, type) : null }>
+                                {!entry.restricted || (entry.restricted && onCallAdministrators.includes(currentUser.user)) ? <entry.icon color={entry.color} size="2.7vw"></entry.icon> : <PadlockIcon color={entry.color} size="2.7vw"></PadlockIcon>}
                                 <div className="on-call-label-container flex-c-col">
                                     <label className="on-call-label">{entry.label.split(" ")[0]}</label>
                                     <label className="on-call-label">{entry.label.split(" ")[1]}</label>
--- infocenter/src/components/OnCall/StaffRoster.js
+++ infocenter/src/components/OnCall/StaffRoster.js
@@ -1,22 +1,10 @@
 import { staffOnCallRoster } from "../../utils/utils"
 
-function getBackgroundColor(index) {
-    if (index % 3 === 0 ) {
-        return {backgroundColor: '#05A475'}
-    }
-    if (index % 3 === 1 ) {
-        return {backgroundColor: '#fa854f'}
-    }
-    else {
-        return {backgroundColor: '#b232d2'} 
-    }
-}
-
 export function StaffRoster() {
     return (
         <div className="modal-display">
             {staffOnCallRoster.map((staff, index) => {
-                const btnStyle = index % 2 === 0 ? "main-link-button" : "alternate-link-button";
+                const btnStyle = index % 2 === 0 ? "main-link-button" : "yellow-green-link-button";
                 return (
                     <div key={`$roster-entry-${index}`} className={`staff-roster-label flex-c ${btnStyle}`}>
                         <label>{`${index + 1}. ${staff}`}</label>
