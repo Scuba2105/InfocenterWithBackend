4f78b8df07ebf1b10aef696634d8b914b99592d8
Steven Bradbury <scuba2150@gmail.com>
2023-11-22 20:31:04 +1100

    refactoring testing templates

--- infocenter/src/components/forms-templates/Testing-Templates/CCUProgress.js
+++ infocenter/src/components/forms-templates/Testing-Templates/CCUProgress.js
@@ -1,15 +1,16 @@
-import { Tick, Cross } from "../../../svg";
 import { useState } from "react";
+import { BedStatusTable } from "./BedStatusTable";
+
 const bedNumbers = [1, 2 , 3, 4, 5, 6, 7, 8];
 
-const testData = [{bed: 1, mx700: true, rack: true, x2: false},
-                {bed: 2, mx700: true, rack: true, x2: false},
-                {bed: 3, mx700: true, rack: true, x2: true},
-                {bed: 4, mx700: true, rack: true, x2: false},
-                {bed: 5, mx700: true, rack: false, x2: false},
-                {bed: 6, mx700: true, rack: true, x2: true},
-                {bed: 7, mx700: true, rack: true, x2: false},
-                {bed: 8, mx700: false, rack: true, x2: true}
+const testData = [{bed: 1, MX700: true, Rack: true, X2: false},
+                {bed: 2, MX700: true, Rack: true, X2: false},
+                {bed: 3, MX700: true, Rack: true, X2: true},
+                {bed: 4, MX700: true, Rack: true, X2: false},
+                {bed: 5, MX700: true, Rack: false, X2: false},
+                {bed: 6, MX700: true, Rack: true, X2: true},
+                {bed: 7, MX700: true, Rack: true, X2: false},
+                {bed: 8, MX700: false, Rack: true, X2: true}
 ]
 
 function updateTestingProgress(testingProgress, setTestingProgress, selectedBedData, device) {
@@ -20,14 +21,22 @@ function updateTestingProgress(testingProgress, setTestingProgress, selectedBedD
     const updatedSelectedBedData = {...selectedBedData};
     updatedSelectedBedData[device] = !affectedDeviceStatus;
 
-    // Map the testing progress to include the updated bed data.
-    const newTestingProgress = testingProgress.map((entry) => {
-        if (entry.bed === updatedSelectedBedData.bed) {
-            return updatedSelectedBedData
-        }
-        return entry
-    })
-    setTestingProgress(newTestingProgress)
+    // Check if the original data for the device is permanent or can be changed.
+    const originalEntry = testData.find((entry) => {
+        return entry.bed === selectedBedData.bed 
+    });
+
+    // If original device status was false then it can be freely changed until uploaded.
+    if (originalEntry[device] === false) {
+        // Map the testing progress to include the updated bed data.
+        const newTestingProgress = testingProgress.map((entry) => {
+            if (entry.bed === updatedSelectedBedData.bed) {
+                return updatedSelectedBedData
+            }
+            return entry
+        })
+        setTestingProgress(newTestingProgress)
+    }    
 }
 
 export function CCUProgress() {
@@ -40,29 +49,10 @@ export function CCUProgress() {
                 const currentBedData = testingProgress.find((bedData) => {
                     return bedData.bed === entry;
                 })
-                
                 return (
-                    <div key={`testing-template-table-${index}`} className="testing-template-table-container size-100 flex-c">
-                        <table className="tg" style={{tableLayout: "fixed", width: 254 + 'px'}}>
-                            <thead>
-                            <tr>
-                                <th className="tg-c3ow" colSpan="3">{`Room ${entry}`}</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td className="tg-c3ow">MX700</td>
-                                <td className="tg-c3ow">Rack</td>
-                                <td className="tg-c3ow">X2</td>
-                            </tr>
-                            <tr>
-                                <td className="tg-c3ow" onClick={() => updateTestingProgress(testingProgress, setTestingProgress, currentBedData, "mx700")}>{currentBedData.mx700 ? <Tick color="rgb(5, 234, 146)"></Tick> : <Cross color="rgb(253, 67, 67)" />}</td>
-                                <td className="tg-c3ow" onClick={() => updateTestingProgress(testingProgress, setTestingProgress, currentBedData, "rack")}>{currentBedData.rack ? <Tick color="rgb(5, 234, 146)"></Tick> : <Cross color="rgb(253, 67, 67)" />}</td>
-                                <td className="tg-c3ow" onClick={() => updateTestingProgress(testingProgress, setTestingProgress, currentBedData, "x2")}>{currentBedData.x2 ? <Tick color="rgb(5, 234, 146)"></Tick> : <Cross color="rgb(253, 67, 67)" />}</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                    <>
+                        <BedStatusTable bedNumber={entry} bedIndex={index} currentBedData={currentBedData} updateTestingProgress={updateTestingProgress} testingProgress={testingProgress} setTestingProgress={setTestingProgress} bedDevices={["MX700", "Rack", "X2"]} />
+                    </>
                 )
             })}
         </div>
