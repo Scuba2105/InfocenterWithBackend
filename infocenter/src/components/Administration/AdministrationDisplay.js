84abf5d078830f682c33f47f0d4e931f7493697e
Steven Bradbury (Hunter New England LHD) <Steven.Bradbury@health.nsw.gov.au>
2023-12-01 16:01:28 +1100

    updates to administartion page

--- infocenter/src/components/Administration/AdministrationDisplay.js
+++ infocenter/src/components/Administration/AdministrationDisplay.js
@@ -1,3 +1,9 @@
+import { serverConfig } from "../../server";
+
+// Store list of Policies and procedures with service request forms.
+const policiesProcedures = {"Acceptance & Commissioning Procedure": `https://${serverConfig.host}:${serverConfig.port}/administration/policies-procedures/Acceptance and Commissioning.pdf`,
+                            "Corrective Maintenance": `https://${serverConfig.host}:${serverConfig.port}/administration/policies-procedures/Corrective_Maintenance.pdf`};
+                               
 export function AdministrationDisplay({queryClient, showMessage, closeDialog}) {
     return (
         <div className="administration-display-container flex-c">
