19e0be636d93980ae9f3897bea6e6d1d0f0df2a8
Steven Bradbury <scuba2150@gmail.com>
2023-12-01 20:59:15 +1100

    started admin templates

--- infocenter/src/components/Administration/AdministrationDisplay.js
+++ infocenter/src/components/Administration/AdministrationDisplay.js
@@ -1,13 +1,30 @@
 import { serverConfig } from "../../server";
 
 // Store list of Policies and procedures with service request forms.
-const policiesProcedures = {"Acceptance & Commissioning Procedure": `https://${serverConfig.host}:${serverConfig.port}/administration/policies-procedures/Acceptance and Commissioning.pdf`,
+const policiesProcedures = {"Acceptance & Commissioning": `https://${serverConfig.host}:${serverConfig.port}/administration/policies-procedures/Acceptance and Commissioning.pdf`,
                             "Corrective Maintenance": `https://${serverConfig.host}:${serverConfig.port}/administration/policies-procedures/Corrective_Maintenance.pdf`};
                                
 export function AdministrationDisplay({queryClient, showMessage, closeDialog}) {
+    
+    const documentsArray = []; 
+    
+    for (const [key, value] of Object.entries(policiesProcedures)) {
+        documentsArray.push(key);
+    }
+    
     return (
-        <div className="administration-display-container flex-c">
-            <h1>Page Not Yet Implemented</h1>
+        <div className="templates-links-container">        
+            {documentsArray.map((entry, index) => {
+                return (
+                    <a key={entry} href={policiesProcedures[entry]} target="_blank" rel="noreferrer" download className={index % 2 === 0 ? "internal-template flex-c-col main-link-button" : "internal-template flex-c-col alternate-link-button"} >
+                        {typeof entry === "string" ? entry : 
+                            entry.map((word, index) => {
+                                return <label key={`${entry}-${index}`}>{word}</label>
+                            })
+                        }
+                    </a>
+                )
+            })}
         </div>
     )
 }
