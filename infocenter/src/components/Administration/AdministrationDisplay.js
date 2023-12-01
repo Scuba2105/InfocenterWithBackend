c41ce50c12ecf25dba3ac8ce097b31a076763ba9
Steven Bradbury <scuba2150@gmail.com>
2023-12-01 22:27:34 +1100

    updates to administration page

--- infocenter/src/components/Administration/AdministrationDisplay.js
+++ infocenter/src/components/Administration/AdministrationDisplay.js
@@ -1,30 +1,26 @@
-import { serverConfig } from "../../server";
+import { PoliciesProcedures } from "./PoliciesProcedures"
+import { ModificationFabrication } from "./ModificationFabrication"
 
-// Store list of Policies and procedures with service request forms.
-const policiesProcedures = {"Acceptance & Commissioning": `https://${serverConfig.host}:${serverConfig.port}/administration/policies-procedures/Acceptance and Commissioning.pdf`,
-                            "Corrective Maintenance": `https://${serverConfig.host}:${serverConfig.port}/administration/policies-procedures/Corrective_Maintenance.pdf`};
-                               
 export function AdministrationDisplay({queryClient, showMessage, closeDialog}) {
     
-    const documentsArray = []; 
-    
-    for (const [key, value] of Object.entries(policiesProcedures)) {
-        documentsArray.push(key);
-    }
-    
     return (
-        <div className="templates-links-container">        
-            {documentsArray.map((entry, index) => {
-                return (
-                    <a key={entry} href={policiesProcedures[entry]} target="_blank" rel="noreferrer" download className={index % 2 === 0 ? "internal-template flex-c-col main-link-button" : "internal-template flex-c-col alternate-link-button"} >
-                        {typeof entry === "string" ? entry : 
-                            entry.map((word, index) => {
-                                return <label key={`${entry}-${index}`}>{word}</label>
-                            })
-                        }
-                    </a>
-                )
-            })}
-        </div>
+        <>
+            <div className="forms-templates-container flex-c-col">
+                <div className="templates-section flex-c-col">
+                    <div className="templates-section-title-container flex-c">
+                        <h2 className="template-heading">Policies and Procedures</h2>
+                    </div>
+                    <PoliciesProcedures />
+                </div>
+            </div>
+            <div className="forms-templates-container flex-c-col">
+                <div className="templates-section flex-c-col">
+                    <div className="templates-section-title-container flex-c">
+                        <h2 className="template-heading">Modification and Fabrication</h2>
+                    </div>
+                    <ModificationFabrication />
+                </div>
+            </div>
+        </>
     )
 }
