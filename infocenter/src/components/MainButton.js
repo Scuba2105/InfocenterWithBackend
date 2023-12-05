730c5ba10d5c2bb4993e2b5c85a662a8af069db1
Steven Bradbury <scuba2150@gmail.com>
2023-12-05 19:07:18 +1100

    finished main buttons api

--- infocenter/src/components/MainButton.js
+++ infocenter/src/components/MainButton.js
@@ -1,6 +1,22 @@
+import { useState } from "react"
+
+function getStyles(buttonClicked, buttonSize, imageColor) {
+    if (buttonClicked) {
+        return { border: `2px solid ${imageColor}` }
+    }
+    return buttonSize === "small" ? { boxShadow: `0px 0px 9px 2px ${imageColor}` } : { boxShadow: `0px 0px 12px 3px ${imageColor}` }
+}
+
+function toggleButtonClick(setButtonClicked) {
+    setButtonClicked(b => !b);
+}
+
 export function MainButton({buttonSize, Image, imageColor, imageSize, onClick, onMouseOver, onMouseOut}) {
+    
+    const [buttonClicked, setButtonClicked] = useState(false);
+
     return (
-        <button className={buttonSize === "small" ? "main-button-small flex-c" : "main-button flex-c"} style={{boxShadow: `-0.9px -0.9px 15px 3px ${imageColor}`}} onClick={onClick} onMouseOver={onMouseOver} onMouseOut={onMouseOut}>
+        <button className={buttonSize === "small" ? "main-button-small flex-c" : "main-button flex-c"} style={getStyles(buttonClicked, buttonSize, imageColor)} onClick={onClick} onMouseOver={onMouseOver} onMouseOut={onMouseOut} onMouseDown={() => toggleButtonClick(setButtonClicked)} onMouseUp={() => toggleButtonClick(setButtonClicked)}>
             <Image color={imageColor} size={imageSize}></Image>
         </button>
     )
