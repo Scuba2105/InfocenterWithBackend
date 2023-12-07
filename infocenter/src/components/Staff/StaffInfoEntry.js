d813d3c7658a7b154553b79131f3dc4ace5adb79
Steven Bradbury <scuba2150@gmail.com>
2023-12-07 18:12:05 +1100

    adding border-shadows for perspective

--- infocenter/src/components/Staff/StaffInfoEntry.js
+++ infocenter/src/components/Staff/StaffInfoEntry.js
@@ -4,7 +4,7 @@ export function StaffInfoEntry({heading, value, Icon, color, bColor}) {
     
     return (
         <div className="info-entry-container flex-c-col">
-            <div className={`info-entry flex-c ${bColor}-link-button`}>
+            <div className={`info-entry flex-c ${bColor}-link-button inactive`}>
                 <Icon color={color} size="6vh"></Icon>
                 <label className="info-entry-label">{heading}</label>
                 {heading === "Computer" ? 
