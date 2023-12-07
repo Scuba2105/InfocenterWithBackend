90cfc71fc9613160048ed178cb61c5bd63005cd3
Steven Bradbury <scuba2150@gmail.com>
2023-12-08 07:03:34 +1100

    updates to css

--- infocenter/src/components/TooltipButton.js
+++ infocenter/src/components/TooltipButton.js
@@ -1,18 +1,30 @@
 import { useState } from "react";
 import { serverConfig } from "../server";
+import { delayFunctionInitiation } from "../utils/utils"
+import tinycolor from "tinycolor2";
 
 function toggleHovered(setHovered) {
-    setHovered(h => !h);   
+    delayFunctionInitiation(() => {
+        setHovered(h => !h);   
+    })
 }
 
 export function TooltipButton({content, boolean, toggleFunction}) {
 
     const [hovered, setHovered] = useState(false);
     
+    // Set the background color
+    const btnColor = boolean ? '#ff69b4' : 'rgb(3, 252, 156)'
+
+    // Get the darker color for the button gradient.
+    const darkBtnColorObject = tinycolor(btnColor).darken(20);
+    const darkBtnColor = (`rgb(${darkBtnColorObject["_r"]}, ${darkBtnColorObject["_g"]}, ${darkBtnColorObject["_b"]})`)
+
+
     return (
         <div className="tooltip-button flex-c-col">
             <div className="tooltip" style={hovered ? {opacity: 1} : {opacity: 0}}>{content}</div>
-            <button className="tooltip-button-img flex-c-col" onMouseOut={() => toggleHovered(setHovered)} onMouseOver={() => toggleHovered(setHovered)} onClick={toggleFunction} style={boolean ? {backgroundColor: '#ff69b4'} : {backgroundColor: 'rgb(3, 252, 156)'}}>
+            <button className="tooltip-button-img flex-c-col form-button-transition" onMouseOut={() => toggleHovered(setHovered)} onMouseOver={() => toggleHovered(setHovered)} onClick={toggleFunction} style={{background: `linear-gradient(${btnColor}, ${darkBtnColor})`}}>
                 <img className="add-new-edit-button" src={boolean ? `https://${serverConfig.host}:${serverConfig.port}/images/undo.svg` : `https://${serverConfig.host}:${serverConfig.port}/images/edit.svg`} alt="edit"></img>
             </button>
         </div>
